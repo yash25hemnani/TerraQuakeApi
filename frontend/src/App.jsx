11da2b830fe3a73328dae2266d5c6f431213e06d
nagcas <studio.nagcas@outlook.it>
2025-07-13 01:38:09 +0200

    update app responsive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import ApiDocsEarthquakes from './components/apiDocs/apiDocsEarthquakes';
 import Hero from './components/hero/hero';
 import Info from './components/info/info';
 import NavbarMenu from './components/navbar/navbarMenu';
@@ -6,10 +7,11 @@ import NavbarMenu from './components/navbar/navbarMenu';
 function App() {
   return (
     <>
-      <div className='relative inset-0 z-10 min-h-screen w-full items-center px-5 py-24 [background:radial-gradient(140%_140%_at_80%_20%,#000_40%,#63e_100%)] scrollbar-thin scrollbar-thumb-purple-700 scrollbar-track-gray-800'>
+      <div className='max-w-full min-h-screen items-center [background:radial-gradient(140%_140%_at_80%_20%,#000_40%,#63e_100%)] scrollbar-thin scrollbar-thumb-purple-700 scrollbar-track-gray-800'>
         <NavbarMenu />
         <Hero />
         <Info />
+        <ApiDocsEarthquakes />
       </div>
     </>
   );
