13933701db11c6471509d5debaf6ea058f478e94
nagcas <studio.nagcas@outlook.it>
2025-07-13 22:06:43 +0200

    add page home

---
+++ frontend/src/pages/home/home.jsx
@@ -0,0 +1,13 @@
+import Hero from '../../components/hero/hero'
+import Info from '../../components/info/info'
+import ApiDocsEarthquakes from '../../components/apiDocs/apiDocsEarthquakes'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Info />
+      <ApiDocsEarthquakes />
+    </>
+  )
+}
