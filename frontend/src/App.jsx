b04a3081eddb95c022c9955c53414b4997239634
nagcas <studio.nagcas@outlook.it>
2025-07-13 22:05:37 +0200

    add route app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css';
+
 import { useEffect } from 'react';
-import ApiDocsEarthquakes from './components/apiDocs/apiDocsEarthquakes';
-import Hero from './components/hero/hero';
-import Info from './components/info/info';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import NavbarMenu from './components/navbar/navbarMenu';
 import createStar from './components/utils/createStar';
+
+import Home from './pages/home/home';
+import ExploreData from './pages/exploreData/exploreData';
+import ApiAccess from './pages/apiAccess/apiAccess';
+import UseCases from './pages/useCases/useCases';
+import About from './pages/about/about';
+import NoPage from './pages/noPage/noPage';
 
 function App() {
 
@@ -17,17 +24,24 @@ function App() {
   }, []);
 
   return (
-    <main>
-      <div className='max-w-full min-h-screen items-center [background:radial-gradient(140%_140%_at_80%_20%,#000_40%,#63e_100%)] scrollbar-thin scrollbar-thumb-purple-700 scrollbar-track-gray-800'>
-        <div className='sky'>
-          <div className='stars'></div>
+    <BrowserRouter>
+      <main>
+        <div className='max-w-full min-h-screen items-center [background:radial-gradient(140%_140%_at_80%_20%,#000_40%,#63e_100%)] scrollbar-thin scrollbar-thumb-purple-700 scrollbar-track-gray-800'>
+          <div className='sky'>
+            <div className='stars'></div>
+          </div>
+          <NavbarMenu />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/explore-data' element={<ExploreData />} />
+            <Route path='/api-access' element={<ApiAccess />} />
+            <Route path='/use-cases' element={<UseCases />} />
+            <Route path='/about' element={<About />} />
+            <Route path='*' element={<NoPage />} />
+          </Routes>
         </div>
-        <NavbarMenu />
-        <Hero />
-        <Info />
-        <ApiDocsEarthquakes />
-      </div>
-    </main>
+      </main>
+    </BrowserRouter>
   );
 }
 
