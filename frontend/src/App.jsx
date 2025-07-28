a0dd3df8a773a1781f4ba172ce6070079666ee03
nagcas <studio.nagcas@outlook.it>
2025-07-29 01:06:15 +0200

    feat: integrate Tailwind CSS into the frontend
    - Added Tailwind CSS and its Vite plugin to package.json dependencies.
    - Updated App.jsx to include a Tailwind styled header.
    - Created index.css to import Tailwind CSS styles.
    - Modified vite.config.js to include Tailwind CSS plugin in Vite configuration.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <>
+      <h1 className='text-red-700 font-semibold'>TerraQuake API</h1>
     </>
   );
 }
 
-export default App;
+export default App
