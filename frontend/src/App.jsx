fbc443832ef7917a5754c7027d5738ff2df77ed5
nagcas <studio.nagcas@outlook.it>
2025-07-29 01:13:45 +0200

    feat: add lucide-react dependency and implement Activity icon in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import { Activity } from 'lucide-react'
 
 function App() {
   return (
     <>
-      <h1 className='text-red-700 font-semibold'>TerraQuake API</h1>
+      <h1 className='flex items-center gap-2 text-red-700 font-semibold'><Activity size={36} /> TerraQuake API</h1>
     </>
   );
 }
---
+++ frontend/src/components/navbar/navbar.jsx
@@ -0,0 +1,9 @@
+export default function navbar() {
+  return (
+    <nav>
+      <div>
+        
+      </div>
+    </nav>
+  )
+}
