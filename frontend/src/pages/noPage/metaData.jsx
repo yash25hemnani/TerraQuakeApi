a527c967a78d90cb2664645eb41aa108ee8b6f96
nagcas <studio.nagcas@outlook.it>
2025-09-16 22:59:57 +0200

    fix: name correct TerraQuake

--- frontend/src/pages/noPage/metaData.jsx
+++ frontend/src/pages/noPage/metaData.jsx
@@ -2,18 +2,18 @@
 export default function MetaData({ title, description }) {
   return (
     <>
-      <title>{`${title} | Terraquake`}</title>
+      <title>{`${title} | TerraQuake`}</title>
       <meta name="description" content={description} />
 
       Open Graph
-      <meta property="og:title" content={`${title} | Terraquake`} />
+      <meta property="og:title" content={`${title} | TerraQuake`} />
       <meta property="og:description" content={description} />
       <meta property="og:type" content="article" />
       <meta property="og:url" content={window.location.href} />
       
 
       Twitter
-      <meta name="twitter:title" content={`${title} | Terraquake`} />
+      <meta name="twitter:title" content={`${title} | TerraQuake`} />
       <meta name="twitter:description" content={description} />
       
     </>
