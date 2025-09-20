383057d55494433100fa1c596fa245402781a055
nagcas <studio.nagcas@outlook.it>
2025-09-21 01:20:29 +0200

    refactor: enhance password reset and forgot password functionality with JWT and improved error handling

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -1,13 +1,8 @@
-import fs from 'fs'
-import path, { dirname } from 'path'
+import dotenv from 'dotenv'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
 
-import { fileURLToPath } from 'url'
-import { UAParser } from 'ua-parser-js'
-import User from '../models/userModels.js'
-import { matchedData } from 'express-validator'
-import handleHttpError from '../utils/handleHttpError.js'
-import nodemailer from 'nodemailer'
-import { buildResponse } from '../utils/buildResponse.js'
+dotenv.config()
 
 /**
  * Controller: Register a new user.
@@ -27,6 +22,7 @@ export const signUp = ({ User, buildResponse, handleHttpError, matchedData, send
       delete user.password
 
       await sendEmailRegister(user)
+
       res.status(200).json(buildResponse(req, 'Registration successful', user, null, {}))
     } catch (error) {
       console.error(error.message)
@@ -76,98 +72,79 @@ export const signIn = ({ User, buildResponse, handleHttpError, tokenSign, matche
 }
 
 // NOTE: Funzione per il recupero della password
-export const forgotPassword = async (req, res) => {
-  try {
-    const validatedData = matchedData(req)
-    const user = await User.findOne({ email: validatedData.email })
-    const userAgent = req.header('User-Agent')
-    const parser = new UAParser(userAgent)
+export const forgotPassword = ({ User, buildResponse, matchedData, handleHttpError, sendForgotPassword }) => {
+  return async (req, res) => {
+    try {
+      req.body = matchedData(req)
 
-    // Create a test account or replace with real credentials. (replace it with actual email in production)
-    const transporter = nodemailer.createTransport({
-      host: 'smtp.ethereal.email',
-      port: 587,
-      auth: {
-        user: 'lonnie.mante78@ethereal.email',
-        pass: 'pvjJDsQyt34Yf5aasx'
+      const user = await User.findOne({ email: req.body.email }).select('email').lean()
+      if (!user) {
+        handleHttpError(res, 'User not found. Please check the email address or register to create an account', 404)
+        return
       }
-    })
 
-    // Emulate __dirname
-    const __filename = fileURLToPath(import.meta.url)
-    const __dirname = dirname(__filename)
-
-    // TODO: CHANGE 'Action URL' to replace the entire URL of password reset when email is set up.
-    // TODO: Update references to support url and other variables in password reset html form as well as text form
-    // Read the HTML file content
-    const htmlPath = path.join(__dirname, '../config/PasswordResetEmailFormat.html')
-    let htmlContent = await fs.promises.readFile(htmlPath, 'utf-8')
-    htmlContent = htmlContent
-      .replace('{{username}}', validatedData.email)
-      .replace('{{operating_system}}', parser.getResult().os.name)
-      .replace('{{browser_name}}', parser.getResult().browser.name)
-      .replace('{{action_url}}', req.originalUrl)
-      .replace('{{support_url}}', 'support@terraquake.com')
-      .replace('{{full_url}}', `${req.protocol}://${req.get('host')}${req.originalUrl}`);
-
-    // Wrap in an async IIFE so we can use await.
-    (async () => {
-      const info = await transporter.sendMail({
-        from: '"TerraQuake Support" <support@terraquake.com>',
-        to: `${req.body.email}`,
-        subject: 'Password Change Request',
-        html: htmlContent // HTML body
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: '15m'
       })
 
-      console.log('Message sent:', info.messageId)
-      console.log(nodemailer.getTestMessageUrl(info))
-    })()
+      await sendForgotPassword(user, token)
 
-    res.status(200).json(buildResponse(req, 'Redirect', user))
-  } catch (error) {
-    console.error(error.message)
-    handleHttpError(res, null, 400)
+      res.status(200).json(buildResponse(req, 'We’ve sent you an email with instructions to reset your password', user, null, {}))
+    } catch (error) {
+      console.error(error.message)
+      handleHttpError(res, null, 400)
+    }
   }
 }
 
 /**
  * Controller: Reset user password.
  *
- * - Looks up the user by email in the database.
- * - Responds with `404 Not Found` if the user does not exist.
- * - Compares the new password with the existing one.
- * - Responds with `409 Conflict` if the new password is the same as the old password.
- * - Updates the user's password and saves the record.
- * - Removes the password field before returning the updated user object.
- * - Responds with `200 OK` and success message on successful update.
- * - Responds with `500 Internal Server Error` if an unexpected error occurs.
+ * - Finds the user by email in the database.
+ * - Returns 404 if the user is not found.
+ * - Checks if the new password matches the old one; returns 409 if it does.
+ * - Updates the user's password and saves it.
+ * - Excludes the password field from the response.
+ * - Returns 200 with success message on success.
+ * - Returns 500 if an unexpected error occurs.
  */
-export const resetPassword = ({ User, handleHttpError, buildResponse, matchedData, compare }) => {
+export const resetPassword = ({ User, handleHttpError, buildResponse }) => {
   return async (req, res) => {
     try {
-      const data = matchedData(req)
-      const { email, password: newUserPassword } = data
+      const { token } = req.params
+      const { password1, password2 } = req.body
 
-      const user = await User.findOne({ email }).select('+password')
+      if (password1 !== password2) {
+        return handleHttpError(res, 'Passwords must match', 400)
+      }
+
+      let decoded
+      try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+      } catch (err) {
+        if (err.name === 'TokenExpiredError') {
+          return handleHttpError(res, 'Reset link expired. Please request a new one.', 401)
+        }
+        return handleHttpError(res, 'Invalid or missing token', 400)
+      }
+
+      const user = await User.findById(decoded.id).select('+password')
       if (!user) {
         return handleHttpError(res, 'User not found', 404)
       }
 
-      const isSame = await compare(newUserPassword, user.password)
+      const isSame = await bcrypt.compare(password1, user.password)
       if (isSame) {
-        return handleHttpError(
-          res,
-          'New password cannot be the same as the old password!',
-          409
-        )
+        return handleHttpError(res, 'New password cannot be the same as the old one!', 409)
       }
 
-      user.password = newUserPassword
-      const savedUser = await user.save()
-      const userResponseObject = savedUser.toObject()
+      user.password = password1
+      await user.save()
+
+      const userResponseObject = user.toObject()
       delete userResponseObject.password
 
-      res.status(200).json(buildResponse(req, 'Password changed successfully!', userResponseObject, null, {}))
+      res.status(200).json(buildResponse(req, 'Password successfully reset!', userResponseObject, null, {}))
     } catch (error) {
       console.error(error.message)
       handleHttpError(res, 'Error resetting password', 500)
