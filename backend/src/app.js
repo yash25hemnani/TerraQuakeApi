419b2c6dd4e017d3b015bb69c60806a4ecf8f641
nagcas <studio.nagcas@outlook.it>
2025-09-21 11:50:14 +0200

    update app style

--- backend/src/app.js
+++ backend/src/app.js
@@ -82,7 +82,7 @@ const startServer = async () => {
       console.log(`Server running in ${devEnv || 'production'} environment`)
       console.log(`Started at: http://localhost:${port}`)
       console.log(`Test: http://localhost:${port}/v1/test`)
-      
+
       const endPoints = expressListEndpoints(app)
       console.log('List of available endpoints:')
       console.table(endPoints)
