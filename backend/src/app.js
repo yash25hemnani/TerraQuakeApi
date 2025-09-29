a573011cfbd9e8cdccf75548246de2073b62a731
Gianluca Chiaravalloti <91532537+nagcas@users.noreply.github.com>
2025-09-29 19:10:41 +0200

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,11 +19,12 @@ const devEnv = process.env.DEV_ENV || 'development'
 const app = express()
 
 // Trust the first proxy (Render)
-app.set('trust proxy', 1);
+app.set('trust proxy', 1)
 
 // === MIDDLEWARE ===
 app.use(helmet())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // === CORS ===
 // Only /v1/earthquakes is public
