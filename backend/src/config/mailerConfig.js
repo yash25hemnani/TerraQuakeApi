0cba533221318ef58707e2ed46b2bbec29b10d03
Gianluca Chiaravalloti <91532537+nagcas@users.noreply.github.com>
2025-09-29 23:27:43 +0200

    Update mailerConfig.js

--- backend/src/config/mailerConfig.js
+++ backend/src/config/mailerConfig.js
@@ -4,23 +4,14 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
+  service: 'gmail',
   auth: {
     user: process.env.USER_MAILER,
     pass: process.env.PASS_MAILER
-  },
-  tls: {
-    rejectUnauthorized: false
-  },
-  connectionTimeout: 10000
+  }
 })
 
 transporter.verify()
-  .then(() => {
-    console.log("Mailer ready to send emails ✅")
-  })
-  .catch((err) => {
-    console.error("Mailer connection error ❌", err)
-  })
+  .then(() => console.log("Mailer ready to send emails ✅"))
+  .catch((err) => console.error("Mailer connection error ❌", err))
+
