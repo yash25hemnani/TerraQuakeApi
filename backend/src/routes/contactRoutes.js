f9c8a77b9c866054f6bf781bb57bc6ec4559240c
nagcas <studio.nagcas@outlook.it>
2025-10-01 10:22:04 +0200

    Implement contact management features with validation and error handling

--- backend/src/routes/contactRoutes.js
+++ backend/src/routes/contactRoutes.js
@@ -6,11 +6,12 @@ import {
   answerContact,
   createContact
 } from '../controllers/contactControllers.js'
+import { validatorContact } from '../validators/contactValidators.js'
 
 const router = express.Router()
 
 // NOTE: Defining a [POST] route to send a new message
-router.post('/create-contact', createContact)
+router.post('/create-contact', validatorContact, createContact)
 
 // NOTE: Defining a [GET] route to display all received messages
 router.get('/get-all-contacts', getAllContacts)
@@ -19,7 +20,7 @@ router.get('/get-all-contacts', getAllContacts)
 router.get('/get-one-contact/:contactId', getOneContact)
 
 // NOTE: Defining a [PATCH] route to reply to a message with a specific id
-router.patch('/answer-contact/:contactId', answerContact)
+router.patch('/answer-contact/:contactId', validatorContact, answerContact)
 
 // NOTE: Defining a [DELETE] route to delete a single message with a specific id
 router.delete('/delete-contact/:contactId', deleteContact)
