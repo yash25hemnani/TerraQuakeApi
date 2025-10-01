02eabdda1adbdec178583eabe9b58ca7a17a8183
nagcas <studio.nagcas@outlook.it>
2025-10-01 19:03:18 +0200

    Implement GitHub authentication and improve error handling in sign-in flow

--- backend/src/controllers/githubController.js
+++ backend/src/controllers/githubController.js
@@ -1,60 +1,64 @@
-/* eslint-disable indent */
 import axios from 'axios'
 import jwt from 'jsonwebtoken'
 import User from '../models/userModels.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const FRONTEND_URL = process.env.FRONTEND_PRODUCTION || 'http://localhost:5173'
 
 const {
-    GITHUB_CLIENT_SECRET,
-    GITHUB_CLIENT_ID,
-    JWT_SECRET,
-    FRONTEND_DEVELOPMENT
+  GITHUB_CLIENT_SECRET,
+  GITHUB_CLIENT_ID,
+  JWT_SECRET
 } = process.env
 
 export const githubAuthController = ({ handleHttpError }) => {
-    console.log('git callback')
-    return async (req, res) => {
-        const code = req.query.code
-        if (!code) return handleHttpError(res, 'Code is required', 400)
+  return async (req, res) => {
+    const code = req.query.code
+    if (!code) return handleHttpError(res, 'Code is required', 400)
 
-        console.log(code, 'code');
+    try {
+      const tokenRes = await axios.post(
+        'https://github.com/login/oauth/access_token',
+        {
+          client_id: GITHUB_CLIENT_ID,
+          client_secret: GITHUB_CLIENT_SECRET,
+          code
+        },
+        { headers: { accept: 'application/json' } }
+      )
 
-        try {
-            const tokenRes = await axios.post(
-                'https://github.com/login/oauth/access_token',
-                { client_id: GITHUB_CLIENT_ID, client_secret: GITHUB_CLIENT_SECRET, code },
-                { headers: { accept: 'application/json' } }
-            )
+      const accessToken = tokenRes.data.access_token
 
-            const accessToken = tokenRes.data.access_token
+      const userRes = await axios.get('https://api.github.com/user', {
+        headers: { Authorization: `Bearer ${accessToken}` }
+      })
 
-            const userRes = await axios.get('https://api.github.com/user', {
-                headers: { Authorization: `Bearer ${accessToken}` }
-            })
+      const githubUser = userRes.data
 
-            const githubUser = userRes.data
+      let user = await User.findOne({ githubId: githubUser.id })
+      if (!user) {
+        user = await User.create({
+          githubId: githubUser.id.toString(),
+          githubUsername: githubUser.login,
+          githubProfileUrl: githubUser.html_url,
+          name: githubUser.name || githubUser.login,
+          avatar: githubUser.avatar_url,
+          role: ['user']
+        })
+      }
 
-            let user = await User.findOne({ githubId: githubUser.id })
-            if (!user) {
-                user = await User.create({
-                    githubId: githubUser.id.toString(),
-                    githubUsername: githubUser.login,
-                    githubProfileUrl: githubUser.html_url,
-                    name: githubUser.name || githubUser.login,
-                    avatar: githubUser.avatar_url,
-                    role: ['user']
-                })
-            }
+      const token = jwt.sign(
+        { _id: user._id, role: user.role || ['user'] },
+        JWT_SECRET,
+        { expiresIn: '7d' }
+      )
 
-            const token = jwt.sign(
-                { _id: user._id, role: user.role || ['user'] },
-                JWT_SECRET,
-                { expiresIn: '7d' }
-            )
-
-            return res.redirect(`${FRONTEND_DEVELOPMENT}/auth/callback?token=${token}`)
-        } catch (err) {
-            console.error('GitHub auth error:', err.message)
-            return handleHttpError(res, 'GitHub login failed', 500)
-        }
+      return res.redirect(`${FRONTEND_URL}/auth/callback?token=${token}`)
+    } catch (err) {
+      console.error('GitHub auth error:', err.message)
+      return handleHttpError(res, 'GitHub login failed', 500)
     }
+  }
 }
--- backend/src/routes/githubAuthRoutes.js
+++ backend/src/routes/githubAuthRoutes.js
@@ -1,20 +1,21 @@
-/* eslint-disable indent */
 import express from 'express'
-import { githubAuthController } from '../controllers/githubController.js';
-import handleHttpError from '../utils/handleHttpError.js'
+import { githubAuthController } from '../controllers/githubController.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const router = express.Router()
 
-const {
-    GITHUB_CLIENT_ID,
-    GITHUB_CALLBACK_URL,
-} = process.env
+const { GITHUB_CLIENT_ID, GITHUB_CALLBACK_URL } = process.env
 
 router.get('/', (req, res) => {
-    const redirectUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${GITHUB_CALLBACK_URL}&scope=read:user`
-    return res.redirect(redirectUrl)
+  const redirectUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${GITHUB_CALLBACK_URL}&scope=read:user`
+  return res.redirect(redirectUrl)
 })
 
-router.get('/callback', githubAuthController(handleHttpError));
+router.get('/callback', githubAuthController({
+  handleHttpError: (res, msg, code = 500) =>
+    res.status(code).json({ success: false, message: msg })
+}))
 
 export default router
